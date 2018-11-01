2c2fd3d93d6b51e17cbb1766f3369834b2b0b30b
Michael Salvati <print@3daddict.com>
2018-11-01 11:13:48 -0700

    working on js function hide/show

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
+const messageSection = document.getElementById('messageSection');
+const messageHeader = document.querySelector('.message-header');
+const messageBody = document.querySelector('.message-body');
 
+messageSection.addEventListener('click', (event) => {
+    if (event.target.style.display == 'none') {
+        messageBody.style.display = 'block';
+    } else {
+        messageBody.style.display = 'none';
+    }
+    
+});
